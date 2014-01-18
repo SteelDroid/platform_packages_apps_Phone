ffd03d16100871a2b9b9882634479554404b027d
Noah Greene <chevycam94@gmail.com>
2014-01-18 00:49:51 -0500

    Initial commit
    Change-Id: I3ef1722365f54dfd1c723142e67218d0672119ba

--- Android.mk
+++ Android.mk
@@ -8,9 +8,9 @@ include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := user
 
 LOCAL_SRC_FILES := \
-	src/com/android/phone/ButtonGridLayout.java \
-	src/com/android/phone/CallLogAsync.java \
-	src/com/android/phone/HapticFeedback.java
+    src/com/android/phone/ButtonGridLayout.java \
+    src/com/android/phone/CallLogAsync.java \
+    src/com/android/phone/HapticFeedback.java
 
 LOCAL_MODULE := com.android.phone.common
 include $(BUILD_STATIC_JAVA_LIBRARY)
@@ -23,9 +23,9 @@ LOCAL_MODULE_TAGS := optional
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_SRC_FILES += \
-        src/com/android/phone/EventLogTags.logtags \
-        src/com/android/phone/INetworkQueryService.aidl \
-        src/com/android/phone/INetworkQueryServiceCallback.aidl
+    src/com/android/phone/EventLogTags.logtags \
+    src/com/android/phone/INetworkQueryService.aidl \
+    src/com/android/phone/INetworkQueryServiceCallback.aidl
 
 LOCAL_PACKAGE_NAME := Phone
 LOCAL_CERTIFICATE := platform
